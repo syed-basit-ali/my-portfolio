99e8f13bb541c43750b07f43f5d9a163456b04dc
syed-basit-ali <basit.shah94@gmail.com>
2023-04-23 11:44:32 +0500

    Feature: static data added

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,18 @@
+export const aboutMeData = [
+  {
+    id: 1,
+    bio: "Hello! I'm a Full stack developer with over 7 years of experience in creating robust and scalable web applications. My expertise lies in building web applications using MongoDB, ExpressJS, ReactJS, and NodeJS.",
+  },
+  {
+    id: 2,
+    bio: "Throughout my career, I have worked on various projects ranging from small business websites to large enterprise applications. I am experienced in designing and developing RESTful APIs, implementing user authentication and authorization, and integrating third-party APIs.",
+  },
+  {
+    id: 3,
+    bio: "I am skilled in using front-end technologies such as HTML, CSS, JavaScript, jQuery, Bootstrap, and Material UI to build responsive and user-friendly interfaces. I also have experience working with Redux, GraphQL, and Apollo Client.",
+  },
+  {
+    id: 4,
+    bio: "I am committed to writing clean and maintainable code that adheres to best practices and industry standards. I have experience working in agile development environments and am comfortable collaborating with cross-functional teams.",
+  },
+];
---
+++ src/data/projects.js
@@ -0,0 +1,43 @@
+// Import images
+// import WebImage1 from "../images/web-project-1.jpg";
+// import WebImage2 from "../images/web-project-2.jpg";
+// import MobileImage1 from "../images/mobile-project-1.jpg";
+// import MobileImage2 from "../images/mobile-project-2.jpg";
+// import UIImage1 from "../images/ui-project-1.jpg";
+// import UIImage2 from "../images/ui-project-2.jpg";
+import Projul from "../images/ProjulConstruction.jpg";
+import Callin from "../images/callin.png";
+import Ozsms from "../images/ozsms.png";
+import OneStreamLive from "../images/OneStreamLive.png";
+
+export const projectsData = [
+  {
+    id: 3,
+    title: "Ozsms",
+    category: "Web Application",
+    img: Ozsms,
+  },
+  {
+    id: 2,
+    title: "Callin",
+    category: "Web Application",
+    img: Callin,
+  },
+  {
+    id: 4,
+    title: "Onestream Live",
+    category: "Web Application",
+    img: OneStreamLive,
+  },
+  {
+    id: 1,
+    title: "Projul",
+    category: "Web Application",
+    img: Projul,
+    ProjectHeader: {
+      title: "Project Management UI - From Context",
+      publishDate: "Jul 26, 2021",
+      tags: "UI / Frontend",
+    },
+  },
+];
